d37bd04e2005c9f6c54d3ea5b83ef40201bbca76
Nikita Sokolov <sokolovniki15@yandex.ru>
2022-10-11 18:43:07 +0300

    7 hw

---
+++ 1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.  /////////...решеная 
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+Console.WriteLine("Ведите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите количество столбов: ");
+int columns= Convert.ToInt32(Console.ReadLine());
+int[,]array = new int [rows, columns];
+
+CreateArray(array);
+PrintArray(array);
+
+void  CreateArray(int [,] array)
+{
+    
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j ] = Convert.ToInt32(new Random().Next(-100,100));
+        }
+    }
+
+   
+} 
+
+void PrintArray(int[,]array)
+{
+for (int i = 0; i < array.GetLength(0); i++ )
+ {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+       
+        Console.Write(array[i,j]+ " ");
+
+    }
+    Console.WriteLine("");
+ }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+///готова 
+
+Console.WriteLine("Ведите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите количество столбов: ");
+int columns= Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[rows, columns];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j ] = Convert.ToInt32(new Random().Next(-100,100));
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
